b0249d1260603e956b1b97a13769f040fb74c8d6
amirzaushnizer <amirzau@wix.com>
2021-12-02 14:49:47 +0200

    add neighbors calc

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Square from "./Square";
 
 interface GameProps {
@@ -14,12 +14,39 @@ class Game extends Component<GameProps> {
     this.bombsMatrix = this.initBombsMatrix();
   }
 
+  getSquareNeighbors = (row: number, col: number) => {
+    const neighbors: number[][] = [];
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = col - 1; j <= col + 1; j++) {
+        if (!(i === row && j === col)) {
+          if (
+            i > -1 &&
+            j > -1 &&
+            i < this.props.gameSize &&
+            j < this.props.gameSize
+          ) {
+            neighbors.push([i, j]);
+          }
+        }
+      }
+    }
+
+    return neighbors;
+  };
+
+  calcNumOfAdjacentBombs = (row: number, col: number) => {
+    const neighbors = this.getSquareNeighbors(row, col);
+    return neighbors.filter(
+      (neighbor) => this.bombsMatrix[neighbor[0]][neighbor[1]]
+    ).length;
+  };
+
   initBombsMatrix = () => {
     const bombsMatrix = Array(this.props.gameSize)
-        .fill(false)
-        .map(() => {
-          return Array(this.props.gameSize).fill(false);
-        });
+      .fill(false)
+      .map(() => {
+        return Array(this.props.gameSize).fill(false);
+      });
 
     for (let i = 0; i < this.props.numOfBombs; i++) {
       const row = Math.floor(Math.random() * this.props.gameSize);
@@ -30,24 +57,25 @@ class Game extends Component<GameProps> {
     return bombsMatrix;
   };
 
-  componentDidMount() {
-    console.log(this.bombsMatrix);
-  }
+  componentDidMount() {}
 
   render() {
     return (
-        <div className="grid-container">
-          {Array.from(Array(this.props.gameSize).keys()).map((i) => (
-              <div className="square-column" key={i}>
-                {Array.from(Array(this.props.gameSize).keys()).map((j) => {
-                  return <Square
-                      key={j}
-                      isBomb={this.bombsMatrix[i][j]}
-                  />;
-                })}
-              </div>
-          ))}
-        </div>
+      <div className="grid-container">
+        {Array.from(Array(this.props.gameSize).keys()).map((i) => (
+          <div className="square-column" key={i}>
+            {Array.from(Array(this.props.gameSize).keys()).map((j) => {
+              return (
+                <Square
+                  key={j}
+                  isBomb={this.bombsMatrix[i][j]}
+                  numOfAdjacentBombs={this.calcNumOfAdjacentBombs(i, j)}
+                />
+              );
+            })}
+          </div>
+        ))}
+      </div>
     );
   }
 }
--- src/Square.tsx
+++ src/Square.tsx
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 
 interface SquareProps {
   isBomb: boolean;
+  numOfAdjacentBombs: number;
 }
 
 class Square extends Component<SquareProps> {
   render() {
-    return <button className="square">{this.props.isBomb ? "X" : ""}</button>;
+    return (
+      <button className="square">
+        {this.props.isBomb
+          ? "X"
+          : this.props.numOfAdjacentBombs > 0
+          ? this.props.numOfAdjacentBombs
+          : ""}
+      </button>
+    );
   }
 }
 
