e40f7931d853e2eceef07a925db4523defaf5ca1
amirzaushnizer <amirzau@wix.com>
2021-12-08 07:48:15 +0200

    open mechanism works

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,41 @@ import Game from "./components/Game";
 interface AppState {
   numOfBombsLeft: number;
   gameSize: number;
-
 }
 
+interface AppProps {}
 
-class App extends React.Component<{}, AppState> {
+class App extends React.Component<AppProps, AppState> {
   constructor(props: {}) {
     super(props);
     this.state = {
       numOfBombsLeft: 15,
       gameSize: 20,
-
     };
   }
 
   setNumOfBombsLeft = (numToAdd: number) => {
     this.setState((prevState) => {
-      return {numOfBombsLeft: prevState.numOfBombsLeft + numToAdd};
+      return { numOfBombsLeft: prevState.numOfBombsLeft + numToAdd };
     });
   };
 
   render() {
-    const {numOfBombsLeft, gameSize} = this.state;
+    const { numOfBombsLeft, gameSize } = this.state;
 
     return (
-        <div className="app-container">
-          <h1>MineSweeper Game</h1>
-          <div className="game-container">
-            <BombsCounter numOfBombsLeft={numOfBombsLeft}/>
-            <Game
-                gameSize={gameSize}
-                numOfBombs={numOfBombsLeft}
-                setNumOfBombsLeft={this.setNumOfBombsLeft.bind(this)}
-            />
-            <Timer/>
-          </div>
+      <div className="app-container">
+        <h1>MineSweeper Game</h1>
+        <div className="game-container">
+          <BombsCounter numOfBombsLeft={numOfBombsLeft} />
+          <Game
+            gameSize={gameSize}
+            numOfBombs={numOfBombsLeft}
+            setNumOfBombsLeft={this.setNumOfBombsLeft.bind(this)}
+          />
+          <Timer />
         </div>
+      </div>
     );
   }
 }
